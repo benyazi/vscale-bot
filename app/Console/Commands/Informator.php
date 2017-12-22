3ff43a050556c8370937db32ff33f9ca9f0071c7
Ben Yazi <benjamin.yazi@gmail.com>
2017-12-22 16:39:15 +0200

    fix telegram cron informator

--- app/Console/Commands/Informator.php
+++ app/Console/Commands/Informator.php
@@ -12,43 +12,46 @@ use Telegram\Bot\Api as TelegramApi;
 
 class Informator extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'vscale:notify';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'vscale:notify';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Check users balance and sent notification';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Check users balance and sent notification';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
 		$users = BotUsers::query()
 			->where('informator_enabled', true)
 			->get();
 		foreach ($users as $user) {
-			$message = '';
+			$this->info("User : " . $user->username);
+			$existMessage = false;
+			$message = 'You have limited balances on:' . PHP_EOL;
 			foreach ($user->keys()->get() as $apiKey) {
+				$this->info("Key : " . $apiKey->api_key);
 				$api = new Api($apiKey->api_key,Api::METHOD_BILLING_BALANCE);
 				if (empty($apiKey->alias)) {
 					$name = "....".substr($apiKey->api_key, -5);
@@ -56,21 +59,26 @@ class Informator extends Command
 					$name = $apiKey->alias;
 				}
 				$result = json_decode($api->request()->getBody(), true);
-				if($result["balance"] < $user->informator_limit) {
+				$this->info("API result : " . print_r($result, true));
+				if( (int) $result["balance"] < (int) $user->informator_limit) {
+					$existMessage = true;
 					$message .= sprintf('*%s* - *%s* RUB' . PHP_EOL,
 						$name,
 						number_format(($result["balance"]/100),2)
 					);
 				}
-				if(!empty($message)) {
-					$telegram = new TelegramApi(env('TELEGRAM_BOT_TOKEN'));
-					$telegram->sendMessage([
-						'chat_id' => $user->chat_id,
-						'text' => $message,
-						'parse_mode' => 'Markdown'
-					]);
-				}
+			}
+			$this->info('Message: ' . $message);
+			if($existMessage) {
+				$telegram_token = config('telegram.bot_token');
+				$telegram = new TelegramApi($telegram_token);
+				$response = $telegram->sendMessage([
+					'chat_id' => $user->chat_id,
+					'text' => $message,
+					'parse_mode' => 'Markdown'
+				]);
+				$this->info("Send message result : " . print_r($response, true));
 			}
 		}
-    }
+	}
 }
